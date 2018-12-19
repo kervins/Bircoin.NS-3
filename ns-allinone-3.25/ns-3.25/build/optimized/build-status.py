38f736601d5dcf2ef548c3964c922f656f275094
kev <kev@someplace.com>
2018-12-19 13:36:15 -0500

    First commit

---
+++ ns-allinone-3.25/ns-3.25/build/optimized/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/optimized/scratch/subdir/ns3.25-subdir-optimized', 'build/optimized/scratch/ns3.25-bitcoin-test-optimized', 'build/optimized/scratch/ns3.25-selfish-miner-test-optimized', 'build/optimized/scratch/ns3.25-scratch-simulator-optimized']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
